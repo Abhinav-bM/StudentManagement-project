5d8f6b60ff6adadb0dda58d66ff7044229bf74a2
Abhinav-bM <abhinavbmanoj@gmail.com>
2024-08-31 22:26:37 +0530

    update

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,1 +1,20 @@
-Y
+let signinPostPage = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    return res
+      .status(200)
+      .json({ success: true, message: "User logged in successfully" });
+  } catch (err) {
+    res.status(500).json("Internal server error");
+  }
+};
